71383d58e60b72747195b6ecd7e7084c7d13eed5
Yu <admin@itc.tokyo>
2022-02-11 12:47:11 +0900

    up to 4-5

--- contracts/zombieattack.sol
+++ contracts/zombieattack.sol
@@ -1,5 +1,13 @@
 import "./zombiehelper.sol";
 
 contract ZombieBattle is ZombieHelper {
-  // ここから始めるのだ
+  uint randNonce = 0;
+  // ここにattackVictoryProbabilityを作成せよ
+
+  function randMod(uint _modulus) internal returns(uint) {
+    randNonce++;
+    return uint(keccak256(now, msg.sender, randNonce)) % _modulus;
+  }
+
+  // ここに新たな関数を作成せよ
 }
